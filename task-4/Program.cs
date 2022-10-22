d0f2a0d825eb8552dac175adbbe32d65b39c3084
Николай Гришин ИФБС-21 <115008137+Grishin-Nikolai-ifbs21@users.noreply.github.com>
2022-10-22 18:32:14 +0400

    Update Program.cs

--- task-4/Program.cs
+++ task-4/Program.cs
@@ -1,12 +1,12 @@
-﻿using LocalLogic;
+using LocalLogic;
 
 int[,] arr = new int[3, 3];
 arr = LocalLogic.Localclass.FillingArray(arr);
 
 Console.WriteLine("Массив: ");
-LocalLogic.Localclass.printArray(arr);
+LocalLogic.Localclass.PrintAray(arr);
 
-int sum = LocalLogic.Localclass.GetSum(arr);
+int sum = LocalLogic.Localclass.GetNegativeSumFromArray(arr);
 
 Console.WriteLine($"Сумма элементов на четных позициях в массиве равна: {sum}");
 
