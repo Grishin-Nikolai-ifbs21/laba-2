7a927be7c965f0ad75c25a78b0a12407adfc372b
Николай Гришин ИФБС-21 <115008137+Grishin-Nikolai-ifbs21@users.noreply.github.com>
2022-10-22 18:33:59 +0400

    Update LocalClass.cs

--- task-7/LocalClass.cs
+++ task-7/LocalClass.cs
@@ -1,32 +1,20 @@
-﻿using System;
+using System;
 
 namespace LocalLogic
 {
     public class LocalClass
     {
-        public static int[] SortArray(int[] arr)
+        public static int[] ArraySortDescending(int[] arr)
         {
-            int[] arr_sort = new int[arr.Length];
+            int[] arr_result = new int[arr.Length];
             for (int i = 0; i < arr.Length; i++)
             {
-                arr_sort[i] = arr[i];
+                arr_result[i] = arr[i];
             }
 
-            int tmp;
-            for (int i = 0; i < arr_sort.Length; i++)
-            {
-                for (int j = i + 1; j < arr_sort.Length; j++)
-                {
-                    if (arr_sort[i] < arr_sort[j])
-                    {
-                        tmp = arr_sort[i];
-                        arr_sort[i] = arr_sort[j];
-                        arr_sort[j] = tmp;
-                    }
-                }
-            }
-
-            return arr_sort;
+            Array.Sort(arr_result);
+            Array.Reverse(arr_result);
+            return arr_result;
         }
     }
 }
